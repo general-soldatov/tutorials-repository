53f8987eb71f383c0c4e3a65d2528d87e16e5096
Юрий Солдатов <141447194+general-soldatov@users.noreply.github.com>
2023-09-19 14:01:52 +0300

    Update phys.py

--- phys.py
+++ phys.py
@@ -1,13 +1,13 @@
 #root.after(1000, click) #бесконечный цикл с прерыванием в секунду click - функция
 
-Resist = 7 #сопротивление с ползунка 0.5...20
+Resist = 7 #сопротивление с ползунка меняем в пределах 0...20
 eta = 0.5 #кпд в момент равенства сопротивлений
 
 butt = False #логическая переменная, когда кнопка не была нажата
 
 butt = not (butt) #при выполнении функции click при нажатии кнопки проводим инверсию логической переменной
 
-def prog_load(butt, Resist):
+def prog_load(butt, Resist): #функция движка лабораторной установки
     in_resist = 3.5  # внутреннее сопротивление источника
     edc = 6  # ЭДС в вольтах
     if butt:  # если кнопка активна
@@ -19,6 +19,6 @@ def prog_load(butt, Resist):
         voltage = edc
     return round(amperage, 3), round(voltage,3)
 
-U, I = prog_load(butt, Resist)
+U, I = prog_load(butt, Resist) #вывод напряжения и силы тока на приборы
 
 print(U, I)
