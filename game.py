9dc8c12124476883ecf8a2121908b77bf9473129
general-soldatov <141447194+general-soldatov@users.noreply.github.com>
2023-10-06 13:31:45 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
+import random
 
+UnknowNumber = random.randint(1,30)
+
+EnterNumber = -1
+
+while EnterNumber != UnknowNumber:
+    EnterNumber = int(input('Введите числе от 1 до 30: '))
+    if EnterNumber < UnknowNumber:
+        print("Введёное число меньше искомого")
+    elif EnterNumber > UnknowNumber:
+        print("Введёное число больше искомого")
+    elif EnterNumber == UnknowNumber:
+        print("Ты угадал, поздравляю!")
