29ba5ad31c106e7e1807d70b40c18d749311dd55
general-soldatov <141447194+general-soldatov@users.noreply.github.com>
2023-11-18 20:38:15 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,34 @@
-import random
+class Vehicle(object):
+    """docstring"""
 
-UnknowNumber = random.randint(1,30)
+    def __init__(self, color, doors, tires):
+        """construtor"""
+        self.color = color
+        self.doors = doors
+        self.tires = tires
 
-EnterNumber = -1
+    def brake(self):
+        return "Braking"
 
-while EnterNumber != UnknowNumber:
-    EnterNumber = int(input('Введите числе от 1 до 30: '))
-    if EnterNumber < UnknowNumber:
-        print("Введёное число меньше искомого")
-    elif EnterNumber > UnknowNumber:
-        print("Введёное число больше искомого")
-    elif EnterNumber == UnknowNumber:
-        print("Ты угадал, поздравляю!")
+
+class Elcar(object):
+
+    def __init__(self, volt, amper, resist):
+        self.volt = volt
+        self.amper = amper
+        self.resist = resist
+
+    def power(self):
+        powers = self.volt * self.amper
+        return "Power = %s" % powers
+
+
+
+
+
+if __name__ == "__main__":
+    car = Vehicle("Red", 4, "car")
+    print(car.brake())
+
+    elcar = Elcar(220, 10, 13)
+    print(elcar.power())
